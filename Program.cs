cb61f12f7057bee30645d24adf441186ec6dd0dd
eilusha <653756918@qq.com>
2020-09-14 00:05:18 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Work1
+{
+    public class PrimeNum
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string a;
+            Console.WriteLine("Please input a number");
+            a = Console.ReadLine();
+            n = int.Parse(a);
+
+            for (int i = 1; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    n = n / i;
+                    Console.WriteLine(i + "is the prime number");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
